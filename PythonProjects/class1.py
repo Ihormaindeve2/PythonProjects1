dd240ad32f8a03923ed6c6cd20844d9701099796
Ihor <romanenkoihor8@gmail.com>
2024-07-26 18:04:14 +0200

    07/2t6

--- PythonProjects/class1.py
+++ PythonProjects/class1.py
@@ -2,3 +2,19 @@ class Person:
     pass #Indicates an empty block 
 p = Person()
 print(p)
+
+class Person1:
+    def sayHi(self):
+        print('Hello, how are you?')
+
+p = Person1()
+p.sayHi()
+
+class Person2:
+    def __init__(self, name):
+        self.name = name
+    def sayHi1(self):
+        print('Hello, my name is:', self.name)
+
+p = Person2('Swaroop')
+p.sayHi1()
